4d5181e00136d059b8742b3c2a1a0d34a924a84b
Ms-WhiteY <akuy_cinna@yahoo.co.jp>
2026-09-14 20:59:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hiragana-practice-v3';
+const CACHE = 'hiragana-practice-v4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
